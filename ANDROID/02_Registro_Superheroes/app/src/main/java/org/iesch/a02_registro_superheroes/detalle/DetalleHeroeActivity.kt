6e0e320597128c60670fc95fd5838ed2e1b8fe7c
unknown <eaznard@iesch.org>
2025-09-30 19:43:40 +0200

    dia 7

--- ANDROID/02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_superheroes/detalle/DetalleHeroeActivity.kt
+++ ANDROID/02_Registro_Superheroes/app/src/main/java/org/iesch/a02_registro_superheroes/detalle/DetalleHeroeActivity.kt
@@ -20,5 +20,21 @@ class DetalleHeroeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //1. Recibimos los objetos del itent
+        //Un objeto es un contenedor de datos que permite almacenar y transportar multiples valores en Actividades o Fragmentos
+        val bundle = intent.extras!!
+        val superHeroeName = bundle.getString("heroName") ?: "No hay nombre"
+        val alterEgo = bundle.getString("alterEgo") ?: "No hay alter ego"
+        val bio = bundle.getString("bio") ?: "No hay bio"
+        val power = bundle.getFloat("power")
+        
+        //2. Rellenamos con los campos que hemos recibido del binding
+        binding.tvHeroNameResult.text = superHeroeName
+        binding.tvAlterEgoResult.text = alterEgo
+        binding.tvBioResult.text = bio
+        binding.ratingBar.rating = power
+            
     }
+
 }
