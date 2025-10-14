8b928594de573c807110bf0d722734bec5e7d84b
unknown <eaznard@iesch.org>
2025-10-14 19:48:44 +0200

    TERMINADA 06

---
+++ ANDROID/06_Ciclo_de_vida/app/src/main/java/org/iesch/app06/MainActivity.kt
@@ -0,0 +1,49 @@
+package org.iesch.app06
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Log.i("ciclovida", "Entramos en el metodo onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.w("ciclovida", "Entramos en el metodo onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("ciclovida", "Entramos en el metodo onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("ciclovida", "Entramos en el metodo onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e("ciclovida", "Entramos en el metodo onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("ciclovida", "Entramos en el metodo onDestroy")
+    }
+}
