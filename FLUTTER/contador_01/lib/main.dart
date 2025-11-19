40f2a98a23bb2cb32771daed63330e8108d0d9a4
unknown <eaznard@iesch.org>
2025-11-19 21:16:38 +0100

    flutter

---
+++ FLUTTER/contador_01/lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:contador/screens/counter_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true, //cambia el boton y lo de arriba
+        colorSchemeSeed: Colors.green
+      ),
+      home: CounterScreen()
+    );
+  }
+}
---
+++ FLUTTER/contador_01/lib/screens/counter_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  const CounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text('Counter Screen'),
+        centerTitle: true,
+      ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('10', style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),),
+              Text('Clicks', style: TextStyle(fontSize: 25),),
+            ],
+        ),
+        ), 
+        floatingActionButton: FloatingActionButton(
+          child: Icon( Icons.add),
+          onPressed: (){
+            
+          }
+          ),
+      );
+  }
+}
